b8dd4cba692211736149ea420591f6c0376cd4f6
plcstevens <philip@tauri-tec.com>
2012-10-29 16:39:13 +0000

    For now simplified to create a new instance on every call. Might be changed later, depending on common cases.

--- lib/comufy.rb
+++ lib/comufy.rb
@@ -11,22 +11,13 @@ require 'logger'
 
 module Comufy
 
-  def self.connector params = {}
-    @connector ||= Connector.new(params)
-  end
-
-  def self.new_connector params = {}
-    @connector = Connector.new(params)
-  end
-
-  def version
-    Comufy::VERSION
+  def self.connect params = {}
+    Connector.new(params)
   end
 
   # Based on Rails implementation, ensures all strings are converted
   # into symbols.
   def symbolize_keys hash
-    return hash unless hash.is_a?(Hash)
     hash.each_with_object({}){|(k,v), h| h[k.to_sym] = v}
   end
 end
